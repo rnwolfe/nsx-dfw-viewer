c721cd1309460a02fcc9cf788318000dbf12f80f
Ryan <rn.wolfe@gmail.com>
2016-10-17 14:08:10 -0400

    working on popover

---
+++ ajax/ipsetDetails.php
@@ -0,0 +1,26 @@
+<?php
+if( ! $_GET['setid'] || ! isset( $_GET['setid']) ) {
+	die("No Set ID provided.");
+} else {
+
+// get functions
+require_once( "../init.php" );
+require_once( "../functions.php" );
+
+// initialize var(s)
+$ipsetId = array();
+
+	$ipsetId = $_GET['setid'];
+	$url = "https://". $nsx_host ."/api/2.0/services/ipset/". $ipsetId;
+	$json = restCall("GET", $url, $nsx_auth, array( "Accept: application/json" ) );
+	$r = json_decode($json);
+
+	$ipSet = json_encode( explode(',', $r->value) );
+
+	 print $ipSet;	
+}
+/* details of ip set:
+Request:
+GET https://<nsxmgr-ip>/api/2.0/services/ipset/<ipset-id>
+*/
+?>
